83e684a24015922008dc0f989991fceba77ce2b3
Quan L. <45319129+Snowflaker1080@users.noreply.github.com>
2025-09-26 11:14:19 +1000

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,15 @@
+# main_app/views.py
+
+from django.shortcuts import render
+from .models import Cat
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cat_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
